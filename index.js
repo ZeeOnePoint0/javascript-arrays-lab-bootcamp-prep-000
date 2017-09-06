62281af614637b6d6ce603a85f37e9d7033db41e
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-06 20:20:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 
-function removeLastKitten() {
+function removeLastKitten(kittens) {
 
   return kittens.slice(0, - kittens.length - 1)
 
