744f57203d56930ec756d0ddd68cdfbd3ff0e828
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-06 19:54:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  array.push(name)
-  return array
+  return array.push('ralph')
 }
